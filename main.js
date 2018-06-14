3941fa9c8d4dacdd694a53527b00066b739927f7
mggb <mathieu.gelbon@hetic.net>
2018-06-14 12:43:27 +0200

    fixex9

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var imgClose = document.querySelector('.img-menu-close');
 var mainnav = document.getElementById('mainav');
 
 
+
 burgerMenu.addEventListener('click',function () {
   mainav.style.display = 'block';
   imgClose.style.display = 'block';
@@ -12,3 +13,18 @@ imgClose.addEventListener('click',function () {
   mainav.style.display = 'none';
   imgClose.style.display = 'none';
 })
+
+
+
+document.addEventListener("wheel", function(event) {
+var position = event.deltaY;
+  document.addEventListener("wheel", function(event) {
+      if (position=== event.deltaY) {
+        console.log('ahhhhhhhh');
+      }
+      if (position > event.deltaY) {
+        console.log('bouhh');
+      }
+
+});
+});
